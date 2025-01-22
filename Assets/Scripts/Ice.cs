01f11f24fe327c79a118cd8e885ab325ad5e0b5e
Yunjae Cho <92170054+NomerColi@users.noreply.github.com>
2025-01-21 21:31:44 -0800

    Make the ice obtainable

--- Assets/Scripts/Ice.cs
+++ Assets/Scripts/Ice.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 public class Ice : MonoBehaviour
 {
     private bool interactable = false;
-
+    private UIManager uimanager;
+    [SerializeField] private int waterAmount = 5;
     public SpriteRenderer hint;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        uimanager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && interactable)
+        {
+            uimanager.AddWater(waterAmount);
+            interactable = false;
+            gameObject.SetActive(false);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col) {
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,18 +6,7 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI waterText;
 
     private int water = 0;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     public void AddWater(int amount)
     {
